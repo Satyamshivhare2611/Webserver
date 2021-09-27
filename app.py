71d271d22fd7f2ad106ffeebddea2084de50764e
satyam <satyamshivhare2611@gmail.com>
2021-09-27 23:06:24 +0530

    second

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def main():
+def webserver():
     return "Hi, This is my First webserver!"
 
 @app.route("/name")
@@ -11,3 +11,4 @@ def name():
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=80)
+
